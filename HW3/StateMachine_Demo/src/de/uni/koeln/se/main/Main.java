40676537ccb51dc0ec27799c99af42d9d683d173
Marius Bernahrndt <bernahrndt@web.de>
2022-10-27 12:52:13 +0200

    Homework

--- HW3/StateMachine_Demo/src/de/uni/koeln/se/main/Main.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/main/Main.java
@@ -1,32 +1,26 @@
 package de.uni.koeln.se.main;
 
-import java.util.Scanner; 
+import java.util.Scanner;
 
 import de.uni.koeln.se.state.Elevator;
 import de.uni.koeln.se.state.Elv_States;
 
 public class Main {
-	public static void main(String[] args) 
-	{
-		
-		 Scanner in = new Scanner(System.in); 
-		
-		System.out.println("Current Floor is 5"); 
+	public static void main(String[] args) {
 
-       
-		System.out.println("Enter destination Floor: (0 to 10):"); 
+		Scanner in = new Scanner(System.in);
 
-		 int dest_floor = in.nextInt(); 
-		 in.close();
-		
-		 System.out.println("--------------------------------------------");
+		System.out.println("Current Floor is 5");
 
-		
-		 Elevator elv=new  Elevator(dest_floor);
+		System.out.println("Enter destination Floor: (0 to 10):");
 
-		
+		int dest_floor = in.nextInt();
+		in.close();
+
+		System.out.println("--------------------------------------------");
+
+		Elevator elv = new Elevator(dest_floor);
+
 	}
-	
-	
 
 }
--- HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
@@ -1,25 +1,35 @@
 package de.uni.koeln.se.state;
 
+public class Elevator {
 
-public class Elevator{
-	
-	public static int current_Floor=5;
+	public static int current_Floor = 5;
 	int dest_Floor;
-	
-	private Elv_States State = new Elv_States();
-	
-	public Elevator (int dest_floor)
-	{
-		this.dest_Floor=dest_floor;
-	System.out.println("*** Destination floor is: "+ dest_Floor);
-	arrive_atFloor();
 
+	private Elv_States State = new Elv_States();
+
+	public Elevator(int dest_floor) {
+		this.dest_Floor = dest_floor;
+		System.out.println("*** Destination floor is: " + dest_Floor);
+		arrive_atFloor();
 
 	}
 
 	private void arrive_atFloor() {
-		
+
+		while (dest_Floor != current_Floor) {
+			if (dest_Floor < current_Floor) {
+				System.out.println(State.Moving_down);
+				current_Floor--;
+			} else if (dest_Floor > current_Floor) {
+				System.out.println(State.Moving_up);
+				current_Floor++;
+			}
+		}
+
+		if (dest_Floor == current_Floor) {
+			System.out.println(State.Idle);
+		}
+
 	}
 
-	
 }
